7f2ae071dd859daa6d1f624720d17f279599fc3e
Merelena <fedorova.elena.dz@gmail.com>
2021-04-29 20:21:12 +0300

    fix migrations

--- db/migrate/20210422165001_add_edu_institution_id.rb
+++
@@ -1,8 +0,0 @@
-class AddEduInstitutionId < ActiveRecord::Migration[6.1]
-  def self.up
-    change_table :users do |t|
-      ## Database authenticatable
-      t.references :edu_institution, foreign_key: { to_table: :edu_institutions }, index: true
-    end
-  end
-end
--- db/migrate/20210424070416_edu_institution_unique_name.rb
+++
@@ -1,5 +0,0 @@
-class EduInstitutionUniqueName < ActiveRecord::Migration[6.1]
-  def change
-    add_index :edu_institutions, :edu_institution, :unique => true
-  end
-end
