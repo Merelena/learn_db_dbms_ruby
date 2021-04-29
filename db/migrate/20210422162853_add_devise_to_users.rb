322eacc5a6ff1a36e89a38f9c1e5c000c9b88fdd
Merelena <fedorova.elena.dz@gmail.com>
2021-04-29 20:13:39 +0300

    fix migration

--- db/migrate/20210422162853_add_devise_to_users.rb
+++ db/migrate/20210422162853_add_devise_to_users.rb
@@ -5,6 +5,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     change_table :users do |t|
       ## Database authenticatable
       t.references :edu_institution, foreign_key: { to_table: :edu_institutions }, index: true
+    end
   end
 
   def self.down
