544b70819dc0280c1889fa4ec3209bc41ba0f954
Merelena <fedorova.elena.dz@gmail.com>
2021-04-30 19:05:42 +0300

    new migrations

--- db/migrate/20210430160231_create_test.rb
+++
@@ -1,9 +0,0 @@
-class CreateTest < ActiveRecord::Migration[6.1]
-  def change
-    create_table :tests do |t|
-      t.string :name, null: false
-      t.references :user, foreign_key: { to_table: :users }, index: true
-      t.timestamps
-    end
-  end
-end
